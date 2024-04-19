ca9a2e8b6e5492afa9cd9f974f78ccccedffd75f
sally <devs2ally@gmail.com>
2024-04-19 22:38:42 +0900

    vue 프로젝트 설치 및 for문 적용

---
+++ vue-form/src/App.vue
@@ -0,0 +1,125 @@
+<template>
+
+  <div class="black-bg" v-if="modal == true">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지내용</p>
+      <button @click="btnClosedModalHandler">x</button>
+    </div>
+  </div>
+
+  <div class="menu">
+    <a v-for="(item, index) in menu" :key="index"> {{ item }} </a>
+  </div>
+  
+  
+  <h1>{{ logo }}</h1>
+  <!-- <div v-for="(item, index) in products" :key="index"> -->
+
+    <div v-for="item in data" :key="item.id">
+      <img src="./assets/room1.jpg" class="room-img">
+      <h4 :style="style" @click="btnModalOpenHandler">{{ item.title }}</h4>
+      <p>{{ item.price }} 만원</p>
+      <p>{{ item.content }}</p>
+    </div>
+
+  <!-- <div>
+    <img src="./assets/room1.jpg" class="room-img">
+    <h4 :style="style" @click="btnModalOpenHandler">{{ products[0] }}</h4>
+    <p>{{ money[0] }} 만원</p>
+    <button @click="btnCountClick(0)">허위매물신고</button>
+    <span>신고수 : {{ countArr[0] }}</span>
+  </div> -->
+  <!-- <div>
+    <img src="./assets/room2.jpg" class="room-img">
+    <h4 @click="btnModalOpenHandler">{{ products[1] }}</h4>
+    <p>{{ money[1] }} 만원</p>
+    <button @click="btnCountClick(1)">허위매물신고</button>
+    <span>신고수 : {{ countArr[1] }}</span>
+  </div> -->
+  <!-- <div>
+    <img src="./assets/room3.jpeg" class="room-img">
+    <h4 @click="btnModalOpenHandler">{{ products[2] }}</h4>
+    <p>{{ money[2] }} 만원</p>
+    <button @click="btnCountClick(2)">허위매물신고</button>
+    <span>신고수 : {{ countArr[2] }}</span>
+  </div> -->
+</template>
+
+<script>
+
+let products = ['역삼동 원룸', '천호동 원룸', '방배동 원룸']
+let money = ['1000', '2000', '3000'];
+
+import data from './data.js';
+// import {apple, apple2} from './data.js';
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      logo : '부동산'
+      , menu : ['Home', 'Shop', 'About']
+      , products : products
+      , money : money
+      , countArr : [0, 0, 0]
+      , modal : false
+      , data : data
+    }
+  },
+  methods : {
+    btnCountClick (index) {
+      this.countArr[index] += 1 //this == data의 object
+    },
+    btnModalOpenHandler () {
+      this.modal = true
+    },
+    btnClosedModalHandler () {
+      this.modal = false
+    }
+    
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
+.room-img {
+  width: 100;
+  margin-top: 40px;
+}
+</style>
---
+++ vue-form/src/data.js
@@ -0,0 +1,82 @@
+
+// var apple = 10;
+// var apple2 = 100;
+
+// export {apple, apple2}
+
+
+const obj = [
+    {
+      "id": 0,
+      "title": "Sinrim station 30 meters away",
+      "image": "./assets/room1.jpg",
+      "content": "18년 신축공사한 남향 원룸, 공기청정기 제공",
+      "price": "500/30"
+    },
+    {
+      "id": 1,
+      "title": "Convenient apartment near downtown",
+      "image": "./assets/room2.jpg",
+      "content": "Cozy apartment with a view, perfect for couples",
+      "price": "600/40"
+    },
+    {
+      "id": 2,
+      "title": "Luxury loft in the heart of the city",
+      "image": "./assets/room3.jpg",
+      "content": "Spacious loft with modern amenities and stylish decor",
+      "price": "800/50"
+    },
+    {
+      "id": 3,
+      "title": "Charming studio near the park",
+      "image": "https://codingapple1.github.io/vue/room3/jpg",
+      "content": "Bright studio apartment with natural light",
+      "price": "450/25"
+    },
+    {
+      "id": 4,
+      "title": "Secluded cottage in the woods",
+      "image": "https://codingapple1.github.io/vue/room4/jpg",
+      "content": "Peaceful retreat surrounded by nature",
+      "price": "700/35"
+    },
+    {
+      "id": 5,
+      "title": "Modern apartment with city views",
+      "image": "https://codingapple1.github.io/vue/room5/jpg",
+      "content": "Newly renovated apartment with stunning skyline views",
+      "price": "550/30"
+    },
+    {
+      "id": 6,
+      "title": "Beachfront villa with private pool",
+      "image": "https://codingapple1.github.io/vue/room6/jpg",
+      "content": "Luxurious villa steps away from the beach",
+      "price": "1200/80"
+    },
+    {
+      "id": 7,
+      "title": "Cozy cabin in the mountains",
+      "image": "https://codingapple1.github.io/vue/room7/jpg",
+      "content": "Rustic cabin with fireplace and mountain views",
+      "price": "600/40"
+    },
+    {
+      "id": 8,
+      "title": "Quaint farmhouse with countryside charm",
+      "image": "https://codingapple1.github.io/vue/room8/jpg",
+      "content": "Traditional farmhouse surrounded by fields and gardens",
+      "price": "500/30"
+    },
+    {
+      "id": 9,
+      "title": "Sleek downtown loft with rooftop terrace",
+      "image": "https://codingapple1.github.io/vue/room9/jpg",
+      "content": "Modern loft apartment with panoramic city views",
+      "price": "750/45"
+    }
+]
+
+
+export default obj;
